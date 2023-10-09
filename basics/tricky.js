c37e8bfdd8ca161b24aefd1fe02bf77886565c17
Rahul Naik <122608184+123RahulNaik@users.noreply.github.com>
2023-10-09 14:20:57 +0530

    Update tricky.js

--- basics/tricky.js
+++ basics/tricky.js
@@ -1,9 +1,22 @@
-let num1, num2, num3;
-let num4 = '', num5 = null;
-num6 = num7 = num8 = 5-2; // not recommended but tricky
-num7++;
-console.log(num1);
-console.log(num4, num5);
-console.log(num6, num7, num8);
+// let num1, num2, num3;
+// let num4 = '', num5 = null;
+// num6 = num7 = num8 = 5-2; // not recommended but tricky
+// num7++;
+// console.log(num1);
+// console.log(num4, num5);
+// console.log(num6, num7, num8);
+
+// Js converts ("5" - 1) this kind automatically..but not for + cause its used as concatenate
+// also for comparison operators
+
+//  console.log(null>0); false... comparitively both are same
+//  console.log(null==0); false.. equally both are not valuely same
+//  console.log(null>=0); true... lets say null is not equally same nor comparitively big or small
+//  console.log(null<=0); true... just it lies between some boundary
 
 
+// here everything is false undefined is considered nothingly
+//  console.log(undefined>0); 
+//  console.log(undefined==0);
+//  console.log(undefined>=0);
+//  console.log(undefined<=0);
